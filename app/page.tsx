1fb0adfa9dd820a94db62eec5a9d2ff5e503469e
Gama Kusumah <gamakusumah@gmail.com>
2023-07-23 13:13:52 +0700

    updated page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -8,32 +8,32 @@ interface Contact {
 }
 
 export default async function Home() {
-  const getContacts = async () => {
-    try {
-      const res: any = await fetch("http://localhost:3000/api/contacts", {
-        cache: "no-store",
-      });
-      if (!res.ok) {
-        throw new Error("Failed to fetch contacts");
-      }
+  // const getContacts = async () => {
+  //   try {
+  //     const res: any = await fetch("http://localhost:3000/api/contacts", {
+  //       cache: "no-store",
+  //     });
+  //     if (!res.ok) {
+  //       throw new Error("Failed to fetch contacts");
+  //     }
 
-      return res.json();
-    } catch (error) {
-      console.log("Error loading contacts : ", error);
-    }
-  };
+  //     return res.json();
+  //   } catch (error) {
+  //     console.log("Error loading contacts : ", error);
+  //   }
+  // };
 
-  const { contacts }: { contacts: Contact[] } = await getContacts();
+  // const { contacts }: { contacts: Contact[] } = await getContacts();
 
   return (
     <main className="p-3 max-w-sm mx-auto">
-      {contacts.map((contact) => (
+      {/* {contacts.map((contact) => (
         <ContactItem
           _id={contact._id}
           name={contact.name}
           phoneNumber={contact.phoneNumber}
         />
-      ))}
+      ))} */}
 
       <AddButton />
     </main>
